edc33da73178f15a123066d16cfbe59ba14b188f
arvin <arvinalejandro@gmail.com>
2024-12-21 23:25:18 +0800

    created UI for login navbar and profile page. and added body parser on BE

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,14 +3,35 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import { connectDB } from './lib/db.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
+    limit: '2mb',
+    extended: false,
+  })
+);
+
+app.use(
+  bodyParser.json({
+    limit: '1mb',
+  })
+);
+
+//app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
